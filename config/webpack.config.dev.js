9eacd09e615388a67281002cc28f95b327aa8340
Chris Jones <chris.r.jones.1983@gmail.com>
2019-04-10 11:27:36 -0500

    change port to 4000 for webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(baseConfig, {
     overlay: true,
     host: '0.0.0.0',
     // default to port 8080 then ++ if non avail
-    // port: 4445
+    port: 4000
     useLocalIp: true, // requires `host:` key
   },
   devtool: 'source-map',
