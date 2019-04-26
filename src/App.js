b026c2ed968982230f8e45b991e538bb980181c5
Chris Jones <chris.r.jones.1983@gmail.com>
2019-04-26 14:51:03 -0500

    add a new component, ie. `MenuHam` for experimenting with css transitions inside react components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
 // libs
 import React from 'react';
-import PropTypes from 'prop-types';
-// components
-import Hello from './components/hello';
-import NavBar from './components/NavBar';
-import MenuIcon from './components/MenuIcon';
-import MenuIconAnimated from './components/MenuIconAnimated';
-//
-import './styles.css';
 import { hot } from 'react-hot-loader';
 // API shit
 import axios from 'axios';
+
+// components
+import MenuHam from './components/MenuHam';
 
 let headers = {
   // withCredentials:true,
   withCredentials:false
 };
 
-// ani-bar init
-const AniBar = ({visible }) => (
-  <div id="ani-bar" className={visible ? 'slideIn' : 'slideOut'}>
-    Animation Bar
-  </div>
-);
-
-AniBar.propTypes = {
-  visible: PropTypes.bool
-};
-
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { visible: false };
-  }
-
-  handleClick() {
-    this.setState(prev => ({ visible: !prev.visible }));
-  }
-
-  
   componentDidMount() {
     axios.get('http://localhost:3000/api/hello', headers)
       .then(function (response) {
@@ -52,21 +25,9 @@ class App extends React.Component {
   }
 
   render() {
-    // print `logging` in a browser console
-    // console.log('logging');
     return (
       <div id="App">
-        <NavBar />
-        <MenuIcon />
-        <MenuIconAnimated />
-        <h1>Hello from App, using React and webpack!</h1>
-        <br />
-        <Hello />
-        <button type="button" onClick={this.handleClick}>
-          {this.state.visible ? 'Hide navbar' : 'Show navbar'}
-        </button>
-        <hr />
-        <AniBar visible={this.state.visible} />
+        <MenuHam />
       </div>
     );
   }
---
+++ src/components/MenuHam.js
@@ -0,0 +1,42 @@
+// libs
+import React from 'react';
+import PropTypes from 'prop-types';
+// styles
+import '../styles.css';
+
+const AniBun = ({visible}) => (
+  // <div id="ani-bun">
+  <div id="bun" className={visible ? 'show' : 'animate'}>
+  </div>
+  // </div>
+);
+
+AniBun.propTypes = {
+  visible: PropTypes.bool
+};
+
+class MenuHam extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { visible: false };
+  }
+
+  handleClick() {
+    this.setState(prev => ({ visible: !prev.visible }));
+  }
+
+  render() {
+    return(
+      <div className="ani-bun">
+        <button type="button" onClick={this.handleClick}>
+          {this.state.visible ? 'animate hamburger' : 'restore hamburger'}
+        </button>
+        {/* <hr /> */}
+        <AniBun visible={this.state.visible} />
+      </div>
+    );
+  }
+}
+
+export default MenuHam;
